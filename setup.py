f17fc6b01b043cb7f0f04638048bf93d24331e3c
William Forde <willforde@gmail.com>
2017-01-11 03:39:40 +0000

    add pip setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='htmlement',
+      version='0.1',
+      description='Python HTMLParser extension with ElementTree support.',
+      url='https://github.com/willforde/python-htmlement',
+      author='William Forde',
+      author_email='willforde@gmail.com',
+      license='MIT',
+      py_modules=['htmlement'],
+      zip_safe=False)
